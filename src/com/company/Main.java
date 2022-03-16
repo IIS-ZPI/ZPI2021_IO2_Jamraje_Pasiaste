5f3f327a66a152595e7e929e7c77a454db38edb2
szatanmilio <jediszatanmilio@gmail.com>
2022-03-16 09:14:52 +0100

    Zadanie 1 wyświetlanie grupy

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Jamraje Pasiaste\nCezary Hanczak - developer\nŁukasz Krulik - tester\nPatryk Kujawiak - developer\nMariusz Sukiennik - tester\nMariusz Walis - operations\nGithub Id Team Leadera - szatanmilio");
+    }
+}
