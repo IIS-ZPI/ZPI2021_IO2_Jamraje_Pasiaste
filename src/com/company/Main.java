d99a0c5c158100f699d1ccba1e318bcd2c747d27
Mariusz.Sukiennik <228858@edu.p.lodz.pl>
2022-03-30 09:11:23 +0200

    Github ID added.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Jamraje Pasiaste\nCezary Hanczak - developer\nŁukasz Krulik - tester\nPatryk Kujawiak - developer\nMariusz Sukiennik - tester\nMariusz Walis - operations\nGithub Id Team Leadera - 94793870");
+        System.out.println("Jamraje Pasiaste\nCezary Hanczak - developer\nŁukasz Krulik - tester\nPatryk Kujawiak - developer\nMariusz Sukiennik, Github ID: 66173536 - tester\nMariusz Walis - operations\nGithub Id Team Leadera - 94793870");
     }
 }
