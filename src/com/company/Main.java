cf96338430966cff4fa786a3f433755b2125c95b
szatanmilio <jediszatanmilio@gmail.com>
2022-03-30 09:06:13 +0200

    Patryk Kujawiak github ID

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Jamraje Pasiaste\nCezary Hanczak - developer\nŁukasz Krulik - tester\nPatryk Kujawiak - developer\nMariusz Sukiennik - tester\nMariusz Walis - operations\nGithub Id Team Leadera - 94793870");
+        System.out.println("Jamraje Pasiaste\nCezary Hanczak - developer\nŁukasz Krulik - tester\nPatryk Kujawiak - developer - Github Id - 94793870\nMariusz Sukiennik - tester\nMariusz Walis - operations\nGithub Id Team Leadera - 94793870");
     }
 }
