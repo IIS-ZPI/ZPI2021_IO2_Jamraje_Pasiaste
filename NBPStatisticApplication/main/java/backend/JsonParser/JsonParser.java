4f5bfa5cfc0a699283fc608ee6992a2291828c59
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-06-05 03:27:09 +0200

    minor refactoring

--- NBPStatisticApplication/main/java/backend/JsonParser/JsonParser.java
+++ NBPStatisticApplication/main/java/backend/JsonParser/JsonParser.java
@@ -19,6 +19,8 @@ import static backend.HTTPrequester.HTTPRequester.getJsonResponseFromURL;
 
 public class JsonParser {
 	//todo work in progress
+	private final static List<String> tables = List.of("http://api.nbp.pl/api/exchangerates/tables/A?format=json", "http://api.nbp.pl/api/exchangerates/tables/B?format=json", "http://api.nbp.pl/api/exchangerates/tables/C?format=json");
+
 	public String getCurrency(JsonResponse response) {
 
 		return response.json().readArray().toString();
@@ -42,23 +44,31 @@ public class JsonParser {
 		String tableName;
 		String effectiveDate;
 		String[] temp;
-		List<String> tables = List.of("http://api.nbp.pl/api/exchangerates/tables/A?format=json", "http://api.nbp.pl/api/exchangerates/tables/B?format=json");
 
 		for (String table : tables) {
 			jsonResponse = getJsonResponseFromURL(table);
 
-			tableName = String.valueOf(jsonResponse.json().readArray().getJsonObject(0).getJsonString("table")).replaceAll("\"", "");
+			tableName = String.valueOf(jsonResponse
+					.json()
+					.readArray()
+					.getJsonObject(0)
+					.getJsonString("table")
+			).replaceAll("\"", "");
 
-			effectiveDate = String.valueOf(jsonResponse.json().readArray().getJsonObject(0).getJsonString("effectiveDate")).replaceAll("\"", "");
+			effectiveDate = String.valueOf(jsonResponse
+					.json()
+					.readArray()
+					.getJsonObject(0)
+					.getJsonString("effectiveDate")
+			).replaceAll("\"", "");
 
 			for (JsonValue array : jsonResponse.json().readArray().getJsonObject(0).getJsonArray("rates")) {
-				temp = array.toString().replaceAll("[\\{:,}]", "").split("\"");
+				temp = array.toString()
+						.replaceAll("[\\{:,}]", "")
+						.split("\"");
 				currencyList.add(new Currency(tableName, temp[3], temp[7], temp[10], effectiveDate));
 			}
-
 		}
-		System.out.println(currencyList.size());
-		System.out.println(currencyList);
 		return currencyList;
 	}
 
